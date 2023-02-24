16e7f29862ac4dbe728983e4daeedd9a7bd53c94
hgtucel <tucelhg@gmail.com>
2023-02-24 17:56:03 +0300

    flutter web cors error fixed

--- lib/business/parser_html.dart
+++ lib/business/parser_html.dart
@@ -7,29 +7,40 @@ import 'package:html/dom.dart';
 import 'package:html/parser.dart' as parser;
 
 class ParserHtml implements IParserHtml {
-  
   @override
   Future<List<EarthquakeModel>> earthquakeInfoList() async {
-    final endpoint = Uri.https("deprem.afad.gov.tr","last-earthquakes.html",);
-    final response = await http.get(endpoint,headers: {"Content-Type" : "text/html; charset=ISO-8859-9"});
+    final endpoint = Uri.https(
+      "deprem.afad.gov.tr",
+      "last-earthquakes.html",
+    );
+    final response = await http.get(endpoint, headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": 'true',
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Content-Type": "text/html; charset=ISO-8859-9",
+    });
 
     Document document = parser.parse(utf8.decode(response.bodyBytes));
     List<Element> tds = document.querySelectorAll(".content-table tbody tr");
     List<Map<String, dynamic>> linkMap = [];
     //print(tds.first.children.first.innerHtml);
-    for(var item in tds){
+    for (var item in tds) {
       linkMap.add({
-        "depth" : item.children[3].innerHtml,
-        "city" : item.children[6].innerHtml.replaceFirst("(", "").replaceAll(")", "").split(" ").last,
-        "size" : num.tryParse(item.children[5].innerHtml),
-        "district" : item.children[6].innerHtml.replaceFirst("(", "").replaceAll(")", "").split(" ").first,
+        "depth": item.children[3].innerHtml,
+        "city": item.children[6].innerHtml
+            .replaceFirst("(", "")
+            .replaceAll(")", "")
+            .split(" ")
+            .last,
+        "size": num.tryParse(item.children[5].innerHtml),
+        "district": item.children[6].innerHtml
+            .replaceFirst("(", "")
+            .replaceAll(")", "")
+            .split(" ")
+            .first,
         "time": item.children.first.innerHtml
       });
     }
     return linkMap.map((item) => EarthquakeModel.fromJson(item)).toList();
-
   }
-
-
-  
 }
